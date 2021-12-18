24361c159e2c9f823a8dd3dfc32bd488cd631c70
seongjiyun <96008057+seongjiyun@users.noreply.github.com>
2021-12-18 23:14:47 +0900

    211218

---
+++ variable.py
@@ -0,0 +1,20 @@
+'''
+#변수 선언과 할당
+pi = 3.14159265
+r = 10
+
+#변수 참조
+print("원주율 = ",pi)
+print("반지름 = ", r)
+print("원의 둘레 = ", 2*pi*r)
+print("원의 넓이 = ", pi*r**2)
+
+'''
+# input("인사말을 입력하세요>")
+
+'''
+string = input("인사말을 입력하세요>")
+print(string)
+'''
+number = input("숫자를 입력하세요>")
+print(number)
