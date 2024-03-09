41077bc474a70a78eba37e6fef49fc245a43bbeb
seongjiyun <96008057+seongjiyun@users.noreply.github.com>
2024-03-09 18:13:36 +0900

    Update and rename P1043_거짓말.java to timeComplexityExample1.java

---
+++ timeComplexityExample1.java
@@ -0,0 +1,14 @@
+public class timeComplexityExample1 {
+	public static void main(Stirng[] args) {
+
+		//1~100 사이 값 랜덤 선택
+		int findNumber = (int)(Math.random() * 100);
+		for(int i = 0; i < 100; i++) {
+			if(i == findNumber) {
+				System.out.println(i);
+				break;
+			}
+		}
+		
+	}
+}
